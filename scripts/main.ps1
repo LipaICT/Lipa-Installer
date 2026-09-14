51cb390efd35225db17a266a9e2881994ac4c858
LipaICT <support@lipa.be>
2026-09-14 09:40:04 +0200

    Update main.ps1

--- scripts/main.ps1
+++ scripts/main.ps1
@@ -19,13 +19,13 @@ function Show-AsciiArt {
 # Function to display the menu
 function Show-Menu {
     Write-Host "`nSelect a script to run:" -ForegroundColor Yellow
-    Write-Host "1. Package Installation Script"
-    Write-Host "2. Endpoint Configuration Script"
-    Write-Host "3. Autotask Ticket Entry Generation Script"
-    Write-Host "4. Move and rename splashtop"
-    Write-Host "5. Update windows"
+    Write-Host "1. Add device to AD domain"
+    Write-Host "2. Package Installation Script HP"
+    Write-Host "3. Package Installation Script Lenovo"
+    Write-Host "4. Endpoint Configuration Script"
+    Write-Host "5. Move and rename splashtop"
     Write-Host "6. Generate Spec Sheet"
-    Write-Host "7. Add device to AD domain"    
+    Write-Host "7. Autotask Ticket Entry Generation Script"
     Write-Host "Enter your choice (1-7): " -NoNewline
 }
 
@@ -38,28 +38,28 @@ while ($true) {
 
     switch ($choice) {
         "1" {
-            Write-Host "Running Package Installation Script..." -ForegroundColor Cyan
-            Invoke-Restmethod "https://raw.githubusercontent.com/LipaICT/Lipa-Installer/refs/heads/main/scripts/package-installation.ps1" | Invoke-Expression
+            Write-Host "Adding device to AD domain..." -ForegroundColor Cyan
+            Invoke-Restmethod "https://raw.githubusercontent.com/LipaICT/Lipa-Installer/refs/heads/main/scripts/domain-add.ps1" | Invoke-Expression
             Read-Host "Press Enter to continue..."
         }
         "2" {
+            Write-Host "Running HP Package Installation Script..." -ForegroundColor Cyan
+            Invoke-Restmethod "https://raw.githubusercontent.com/LipaICT/Lipa-Installer/refs/heads/main/scripts/package-installation.ps1" | Invoke-Expression
+            Read-Host "Press Enter to continue..."
+        }
+        "3" {
+            Write-Host "Running Lenovo Package Installation Script..." -ForegroundColor Cyan
+            Invoke-Restmethod "https://raw.githubusercontent.com/LipaICT/Lipa-Installer/refs/heads/main/scripts/Install-Packages-Lenovo.ps1" | Invoke-Expression
+            Read-Host "Press Enter to continue..."
+        }
+        "4" {
             Write-Host "Running Endpoint Configuration Script..." -ForegroundColor Cyan
             Invoke-Restmethod "https://raw.githubusercontent.com/LipaICT/Lipa-Installer/refs/heads/main/scripts/endpoint-configuration.ps1" | Invoke-Expression
             Read-Host "Press Enter to continue..."
         }
-        "3" {
-            Write-Host "Running Autotask Ticket Entry Generation Script..." -ForegroundColor Cyan
-            Invoke-Restmethod "https://raw.githubusercontent.com/LipaICT/Lipa-Installer/refs/heads/main/scripts/generate-ticket.ps1" | Invoke-Expression
-            Read-Host "Press Enter to continue..."
-        }
-         "4" {
+        "5" {
             Write-Host "Running move and rename splashtop..." -ForegroundColor Cyan
             Invoke-Restmethod "https://raw.githubusercontent.com/LipaICT/Lipa-Installer/refs/heads/main/scripts/rename-and-move-splashtop.ps1" | Invoke-Expression
-            Read-Host "Press Enter to continue..."
-        }
-         "5" {
-            Write-Host "Running windows updates..." -ForegroundColor Cyan
-            Invoke-Restmethod "https://raw.githubusercontent.com/LipaICT/Lipa-Installer/refs/heads/main/scripts/windows-update.ps1" | Invoke-Expression
             Read-Host "Press Enter to continue..."
         }
         "6" {
@@ -68,14 +68,18 @@ while ($true) {
             Read-Host "Press Enter to continue..."
         }
         "7" {
-            Write-Host "Adding device to AD domain..." -ForegroundColor Cyan
-            Invoke-Restmethod "https://raw.githubusercontent.com/LipaICT/Lipa-Installer/refs/heads/main/scripts/domain-add.ps1" | Invoke-Expression
+            Write-Host "Running Autotask Ticket Entry Generation Script..." -ForegroundColor Cyan
+            Invoke-Restmethod "https://raw.githubusercontent.com/LipaICT/Lipa-Installer/refs/heads/main/scripts/generate-ticket.ps1" | Invoke-Expression
             Read-Host "Press Enter to continue..."
         }
         default {
-            Write-Host "Invalid choice. Please enter a number between 1 and 4." -ForegroundColor Red
+            Write-Host "Invalid choice. Please enter a number between 1 and 7." -ForegroundColor Red
             Read-Host "Press Enter to continue..."
         }
     }
+    
+    Clear-Host
+}
+    
     Clear-Host
 }
